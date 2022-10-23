9e225a8802d0422aa8a9914ad90f5c75fe4ea35c
Scallium <88222543+Scallium@users.noreply.github.com>
2022-10-23 13:21:34 -0700

    Update GetTraceback.lua

--- Debug/GetTraceback.lua
+++ Debug/GetTraceback.lua
@@ -1,6 +1,7 @@
 ---Helper function to get a stack trace.
 ---
 ---This will only work if the `--luadebug` launch option is enabled.
+---@return string
 function TSIL.Debug.GetTraceback()
     if debug ~= nil then
         return debug.traceback()
