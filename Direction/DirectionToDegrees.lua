dd6bc72f6197d461de2876a1060ad0d5a80d0398
thicco-catto <cristobal.martin01@gmail.com>
2022-10-25 16:06:57 +0200

    feat: Direction functions

---
+++ Direction/DirectionToDegrees.lua
@@ -0,0 +1,14 @@
+local DIRECTION_TO_ANGLE = {
+    [Direction.NO_DIRECTION] = 0,
+  [Direction.LEFT] = 180,
+  [Direction.UP] = 270,
+  [Direction.RIGH] = 0,
+  [Direction.DOWN] = 90,
+}
+
+--- Helper function to get the corresponding angle degrees from a `Direction` enum.
+---@param direction Direction
+---@return integer
+function TSIL.Direction.DirectionToDegrees(direction)
+    return DIRECTION_TO_ANGLE[direction]
+end
