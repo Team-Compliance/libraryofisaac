bf4e051fc181a814f40e0cf46216d7390ee8dc68
thicco-catto <cristobal.martin01@gmail.com>
2023-01-19 12:56:51 +0100

    feat: Added Pickup Init First callback

---
+++ CustomCallbacks/PickupCallbacks/PickupInitFirstCallback.lua
@@ -0,0 +1,29 @@
+--##POST_PICKUP_INIT_FIRST
+
+TSIL.__RegisterCustomCallback(
+    TSIL.Enums.CustomCallback.POST_PICKUP_INIT_FIRST,
+    TSIL.Enums.CallbackReturnMode.NONE,
+    TSIL.Enums.CallbackOptionalArgType.ENTITY_TYPE_VARIANT_SUBTYPE
+)
+
+
+---@param pickup EntityPickup
+local function OnPickupInit(_, pickup)
+    local room = Game():GetRoom()
+    local roomDesc = TSIL.Rooms.GetRoomDescriptor()
+
+    local roomFrameCount = room:GetFrameCount()
+    local visitedCount = roomDesc.VisitedCount
+
+    if roomFrameCount > 0 or visitedCount == 0 then
+        TSIL.__TriggerCustomCallback(
+            TSIL.Enums.CustomCallback.POST_PICKUP_INIT_FIRST,
+            pickup
+        )
+    end
+end
+TSIL.__AddInternalCallback(
+    "PICKUP_INIT_FIRST_CALLBACK_ON_PICKUP_INIT",
+    ModCallbacks.MC_POST_PICKUP_INIT,
+    OnPickupInit
+)
