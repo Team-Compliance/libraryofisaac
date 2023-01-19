1729633da965b3eb239f103079caf0ccd6928a8e
thicco-catto <cristobal.martin01@gmail.com>
2023-01-19 12:19:14 +0100

    feat: Added NPC State Changed callback

---
+++ CustomCallbacks/NPCCallbacks/NPCStateChangedCallback.lua
@@ -0,0 +1,45 @@
+--##POST_NPC_STATE_CHANGED
+TSIL.__RegisterCustomCallback(
+    TSIL.Enums.CustomCallback.POST_NPC_STATE_CHANGED,
+    TSIL.Enums.CallbackReturnMode.NONE,
+    TSIL.Enums.CallbackOptionalArgType.ENTITY_TYPE_VARIANT_SUBTYPE
+)
+
+
+local function OnTSILLoad()
+    TSIL.SaveManager.AddPersistentVariable(TSIL.__MOD, "npcStatesLastFrame_NPC_STATE_CHANGE_CALLBACK", {}, TSIL.Enums.VariablePersistenceMode.RESET_ROOM)
+end
+TSIL.__AddInternalCallback(
+    "NPC_STATE_CHANGED_CALLBACK_ON_TSIL_LOAD",
+    TSIL.Enums.CustomCallback.POST_TSIL_LOAD,
+    OnTSILLoad
+)
+
+
+---@param npc EntityNPC
+local function OnNPCUpdate(_, npc)
+    local npcPtr = GetPtrHash(npc)
+    local npcPtrStr = tostring(npcPtr)
+
+    local npcStatesLastFrame = TSIL.SaveManager.GetPersistentVariable(TSIL.__MOD, "npcStatesLastFrame_NPC_STATE_CHANGE_CALLBACK")
+
+    local npcStateLastFrame = npcStatesLastFrame[npcPtrStr]
+    local npcStateCurrentFrame = npc.State
+
+    if npcStateLastFrame ~= nil and
+    npcStateLastFrame ~= npcStateCurrentFrame then
+        TSIL.__TriggerCustomCallback(
+            TSIL.Enums.CustomCallback.POST_NPC_STATE_CHANGED,
+            npc,
+            npcStateLastFrame,
+            npcStateCurrentFrame
+        )
+    end
+
+    npcStatesLastFrame[npcPtrStr] = npcStateCurrentFrame
+end
+TSIL.__AddInternalCallback(
+    "NPC_STATE_CHANGED_CALLBACK_ON_EFFECT_UPDATE",
+    ModCallbacks.MC_NPC_UPDATE,
+    OnNPCUpdate
+)
