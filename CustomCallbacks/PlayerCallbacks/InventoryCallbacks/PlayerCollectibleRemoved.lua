2d3bfd190e79a221de1bda82e04af02008060cea
thicco-catto <cristobal.martin01@gmail.com>
2023-04-05 17:12:18 +0200

    fix: Fixed Collectible/Trinket Added/Removed callbacks taking the optional arguments

--- CustomCallbacks/PlayerCallbacks/InventoryCallbacks/PlayerCollectibleRemoved.lua
+++ CustomCallbacks/PlayerCallbacks/InventoryCallbacks/PlayerCollectibleRemoved.lua
@@ -4,6 +4,6 @@
 TSIL.__RegisterCustomCallback(
 	TSIL.Enums.CustomCallback.POST_PLAYER_COLLECTIBLE_REMOVED,
 	TSIL.Enums.CallbackReturnMode.NONE,
-	TSIL.Enums.CallbackOptionalArgType.PLAYER_TYPE,
+	TSIL.Enums.CallbackOptionalArgType.PLAYER_TYPE_VARIANT,
 	TSIL.Enums.CallbackOptionalArgType.GENERIC
 )
--- CustomCallbacks/PlayerCallbacks/InventoryCallbacks/PlayerGulpedTrinketRemoved.lua
+++ CustomCallbacks/PlayerCallbacks/InventoryCallbacks/PlayerGulpedTrinketRemoved.lua
@@ -4,6 +4,6 @@
 TSIL.__RegisterCustomCallback(
 	TSIL.Enums.CustomCallback.POST_PLAYER_GULPED_TRINKET_REMOVED,
 	TSIL.Enums.CallbackReturnMode.NONE,
-	TSIL.Enums.CallbackOptionalArgType.PLAYER_TYPE,
+	TSIL.Enums.CallbackOptionalArgType.PLAYER_TYPE_VARIANT,
 	TSIL.Enums.CallbackOptionalArgType.GENERIC
 )
