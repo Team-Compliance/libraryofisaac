8cadb90eef50f17299e55c2bed0cd96590f30b9e
thicco-catto <cristobal.martin01@gmail.com>
2023-04-28 10:48:52 +0200

    feat: Added RunInFramesTemporary function

--- Utils/Functions/Scheduler.lua
+++ Utils/Functions/Scheduler.lua
@@ -1,7 +1,11 @@
 local ScheduledFunctions = {}
+local TemporaryScheduledFunctions = {}
 
-local function OnFrameUpdate()
-	TSIL.Utils.Tables.ForEach(ScheduledFunctions, function (_, scheduledFunction)
+
+---@param functions table
+---@return table
+local function UpdateScheludedFunctions(functions)
+	TSIL.Utils.Tables.ForEach(functions, function (_, scheduledFunction)
 		scheduledFunction.frames = scheduledFunction.frames - 1
 
 		if scheduledFunction.frames <= 0 then
@@ -9,14 +13,41 @@ local function OnFrameUpdate()
 		end
 	end)
 
-	ScheduledFunctions = TSIL.Utils.Tables.Filter(ScheduledFunctions, function (_, scheduledFunction)
+	return TSIL.Utils.Tables.Filter(functions, function (_, scheduledFunction)
 		return scheduledFunction.frames > 0
 	end)
+end
+
+
+local function OnFrameUpdate()
+	ScheduledFunctions = UpdateScheludedFunctions(ScheduledFunctions)
+	TemporaryScheduledFunctions = UpdateScheludedFunctions(TemporaryScheduledFunctions)
 end
 TSIL.__AddInternalCallback(
 	"SCHEDULER_POST_UPDATE",
 	ModCallbacks.MC_POST_UPDATE,
 	OnFrameUpdate
+)
+
+
+local function OnNewRoom()
+	TemporaryScheduledFunctions = {}
+end
+TSIL.__AddInternalCallback(
+	"SCHEDULER_POST_NEW_ROOM",
+	ModCallbacks.MC_POST_NEW_ROOM,
+	OnNewRoom
+)
+
+
+local function OnGameExit()
+	ScheduledFunctions = {}
+	TemporaryScheduledFunctions = {}
+end
+TSIL.__AddInternalCallback(
+	"SCHEDULER_PRE_GAME_EXIT",
+	ModCallbacks.MC_PRE_GAME_EXIT,
+	OnGameExit
 )
 
 
@@ -27,4 +58,16 @@ TSIL.__AddInternalCallback(
 function TSIL.Utils.Functions.RunInFrames(funct, frames, ...)
 	local args = {...}
 	table.insert(ScheduledFunctions, {funct = funct, frames = frames, params = args})
+end
+
+
+--- Runs a function in a given number of frames.
+--- 
+--- Unlike `TSIL.Utils.Functions.RunInFrames` this will not persist when moving to a different room.
+---@param funct function
+---@param frames integer
+---@param ... any
+function TSIL.Utils.Functions.RunInFramesTemporary(funct, frames, ...)
+	local args = {...}
+	table.insert(TemporaryScheduledFunctions, {funct = funct, frames = frames, params = args})
 end
