2c373ae4621585669ba590df7122c88c62f949b9
thicco-catto <cristobal.martin01@gmail.com>
2023-04-28 10:52:59 +0200

    feat: Added Pre Get Collectible Custom Item Pool callback

---
+++ CustomCallbacks/CollectibleCallbacks/GetCollectibleCustomItemPool.lua
@@ -0,0 +1,8 @@
+--##POST_GET_COLLECTIBLE_CUSTOM_ITEM_POOL
+
+TSIL.__RegisterCustomCallback(
+    TSIL.Enums.CustomCallback.POST_GET_COLLECTIBLE_CUSTOM_ITEM_POOL,
+    TSIL.Enums.CallbackReturnMode.NEXT_ARGUMENT,
+    TSIL.Enums.CallbackOptionalArgType.GENERIC,
+    TSIL.Enums.CallbackOptionalArgType.GENERIC
+)
